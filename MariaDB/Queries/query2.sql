eb79793e7e3fc7431d47e9132033f3ec0a2a01ae
sioros <antonios.sioros@nokia.com>
2024-05-26 14:32:41 +0300

    AdD more ole ole

--- MariaDB/Queries/query2.sql
+++ MariaDB/Queries/query2.sql
@@ -7,4 +7,4 @@ FROM Cook c
          JOIN EpisodeParticipation ep ON c.CookID = ep.CookID
          JOIN Recipe r ON ep.RecipeID = r.RecipeID
          JOIN Episode e ON ep.EpisodeID = e.EpisodeID
-WHERE r.NationalCuisine = 'Mexican' AND e.Year = 2023;
+WHERE r.NationalCuisine = 'Mexican' AND e.Year = 2003;
